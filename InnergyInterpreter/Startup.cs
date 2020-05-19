c1e751f83e064b51842fae27c7b73dcec42030b8
Paweł Jarecki <paweljarec@gmail.com>
2020-05-19 15:36:05 +0200

    add swagger

--- InnergyInterpreter/Startup.cs
+++ InnergyInterpreter/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace InnergyInterpreter
 {
@@ -26,15 +27,27 @@ namespace InnergyInterpreter
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "INNERGY Interpreter API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "INNERGY Interpreter API V1");
+            });
 
             app.UseHttpsRedirection();
 
